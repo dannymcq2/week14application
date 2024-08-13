4799678bb8075a4b26a5151624143174a8e0541c
dannymcq2 <166949494+dannymcq2@users.noreply.github.com>
2024-08-13 01:41:41 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,10 @@
-// config/connection.js
-
 const Sequelize = require('sequelize');
 require('dotenv').config();
+
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
